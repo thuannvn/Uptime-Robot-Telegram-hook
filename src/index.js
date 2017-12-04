716083c93f1fa560731ad1d52f74a0d5aaa1c83c
Thuan Nguyen <ninhthuan82us@gmail.com>
2017-12-04 14:22:04 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.get("/", (req,res) => {
 });
 
 app.post("/", jsonParser, (req, res) => {
-  console.log(req);
+  console.log(req.body);
   
   if (!req.body || req.body.key !== config.hookKey) return res.sendStatus(400)
   telegramEmitter(config.telegramBotToken, config.telegramChatId, req.body);
